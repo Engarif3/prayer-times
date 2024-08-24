ef4e142ef2591f1bb45394dc639aad814f495e4c
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-08-24 12:52:49 +0200

    script fixed in package.json file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import { resolvers } from "./resolvers";
 import { PrismaClient, Prisma } from "@prisma/client";
 import { DefaultArgs } from "@prisma/client/runtime/library";
 import { jwtToken } from "./utils/jwtToken";
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 interface Context {
   prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
--- backend/src/resolvers/prayer.ts
+++ backend/src/resolvers/prayer.ts
@@ -1,9 +1,7 @@
+import { userLoader } from "../dataLoaders/userLoaders";
+
 export const Prayer = {
   author: async (parent: any, args: any, { prisma, userInfo }: any) => {
-    return await prisma.user.findUnique({
-      where: {
-        id: parent.authorId,
-      },
-    });
+    return userLoader.load(parent.authorId);
   },
 };
