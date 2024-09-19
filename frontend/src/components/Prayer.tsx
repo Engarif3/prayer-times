5b11a91241f6301f9d907251ae7a60f6ffcd6ef0
Md Arifur Rahman <arif.aust.eng@gmail.com>
2024-09-19 11:03:19 +0200

    margin padding solved for small devices

--- frontend/src/components/Prayer.tsx
+++ frontend/src/components/Prayer.tsx
@@ -2,38 +2,29 @@
 // https://random-hadith-generator.vercel.app/
 // https://documenter.getpostman.com/view/5211979/TVev3j7q#intro   (2932-3162)
 import { useEffect, useMemo, useState } from "react";
+import fajr from "../assets/fajr.jpg";
+import dhuhr from "../assets/dhuhr.jpg";
 import Asr from "../assets/asr.jpg";
 import Maghrib from "../assets/magrib.webp";
+import isha from "../assets/isha.jpg";
+import jumuah from "../assets/jumuah.jpg";
 
 interface PrayerProps {
   title: string;
   times: { title: string; prayerTime: string }[];
 }
 
-const Fajr = "https://farm3.staticflickr.com/2940/14111317317_1638c4916b_c.jpg";
-const Dhuhr =
-  "https://www.agoda.com/wp-content/uploads/2019/06/shutterstock_721552321-1024x683.jpg";
-// const Asr =
-//   "https://cdn.pixabay.com/photo/2023/10/20/03/19/mosque-8328077_1280.jpg";
-// const Maghrib =
-//   "https://pixabay.com/photos/travel-tourism-abu-dhabi-mosque-4604499/";
-// "https://img.freepik.com/free-photo/majestic-mosque-islamic-new-year-celebration-with-fantasy-architecture_23-2151457458.jpg?t=st=1724512989~exp=1724516589~hmac=8b21dfd0e75ec7f6a91a4c5a1785fc476636a6222bcbe8343ecbfb43b056cb1d&w=1380";
-const Isha =
-  "https://www.housingdiversitynetwork.co.uk/wp-content/uploads/crescent-shaped-moon-mosque-front-night-cloudy-starry-sky-ramadan-2048x1367.jpg";
-const DefaultImage =
-  "https://citycentralmosque.org/wp-content/uploads/2021/10/Jum-700x450.jpg";
-
 const Prayer = ({ title, times }: PrayerProps) => {
   const imageMap: { [key: string]: string } = {
-    Fajr: Fajr,
-    Dhuhr: Dhuhr,
+    Fajr: fajr,
+    Dhuhr: dhuhr,
     Asr: Asr,
     Maghrib: Maghrib,
-    Isha: Isha,
-    "Jumu'ah": Dhuhr,
+    Isha: isha,
+    "Jumu'ah": jumuah,
   };
 
-  const imageUrl = imageMap[title] || DefaultImage;
+  const imageUrl = imageMap[title] || jumuah;
 
   const createPrayerDate = (prayerTime: string, date: Date) => {
     const [hours, minutes] = prayerTime.split(":").map(Number);
